edd339e424993dc76f2ab9e9b5b7a05b1e50e943
MatheusGoncalves540 <matheussouzagoncalves540@gmail.com>
2023-05-12 14:54:33 -0300

    init banco

---
+++ banco.py
@@ -0,0 +1,8 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column()
