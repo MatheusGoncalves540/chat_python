4b61b66c61a05ef0accaa84b13d8f79ef6da6ed7
MatheusGoncalves540 <matheussouzagoncalves540@gmail.com>
2023-05-12 14:54:47 -0300

    update

--- servidor.py
+++ servidor.py
@@ -1,7 +1,7 @@
 import socket
 
 servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-servidor.bind('localhost', 8080)
+servidor.bind(('localhost', 8080))
 
 servidor.listen()
 cliente, end = servidor.accept()
