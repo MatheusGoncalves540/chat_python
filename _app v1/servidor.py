838fc817203b12bb498a75f8173a704248fe3bfd
Dionizio Ferreira <dionizioaf@gmail.com>
2023-05-13 18:39:21 -0300

    upgrade

--- _app v1/servidor.py
+++ _app v1/servidor.py
@@ -4,8 +4,9 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy import Column, Integer, String, TIMESTAMP
 import datetime
 import json
+from threading import Thread
 
-database = sqlalchemy.create_engine('sqlite:///db.db', echo=True)
+database = sqlalchemy.create_engine('sqlite:///db.db', echo=False)
 declarativeBase = declarative_base()
 
 #classe de usuario
@@ -20,30 +21,68 @@ class User(declarativeBase):
 declarativeBase.metadata.create_all(database)
 
 servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-servidor.bind(('localhost', 8080))
+servidor.bind(('localhost', 8081))
 
 servidor.listen()
 cliente, end = servidor.accept()
 
 username = "SERVER"
 
-while True:
-    #recebendo msg do cliente
-    msg = json.loads(cliente.recv(10240).decode('utf-8'))
-    print(f"{msg['name']}: {msg['msg']}, Horário: {msg['hora']}")
-    print(datetime.datetime.strptime(msg['hora'],'%Y-%m-%d %H:%M:%S.%f'))
+database = sqlalchemy.create_engine('sqlite:///db.db', echo=True)
+declarativeBase = declarative_base()
 
-    #enviando msg pro cliente
-    server_input = input('Resposta: ')
-    msg_env = '{' + f''' "name":"{username}",
-               "msg":"{server_input}",
+msg_env = '{' + f''' "name":"{username}",
+               "msg":"Dionizio é incrivel",
                "hora":"{datetime.datetime.now()}"
                ''' + '}'
-    cliente.send(msg_env.encode('utf-8'))
+cliente.send(msg_env.encode('utf-8'))
+
+
+def enviar():
+    while True:
+        server_input = input('Resposta: ')
+        msg_env = '{' + f''' "name":"{username}",
+                "msg":"{server_input}",
+                "hora":"{datetime.datetime.now()}"
+                ''' + '}'
+        cliente.send(msg_env.encode('utf-8'))
+        # SALVA NO BANCO
+def receber():
+    while True:
+        msg = json.loads(cliente.recv(10240).decode('utf-8'))
+        print(f"{msg['name']}: {msg['msg']}, Horário: {msg['hora']}")
+        # cliente.send(msg.encode('utf-8'))
+        # SALVA NO BANCO
+
+
+
+r = Thread(target=receber) #CRIA EM MEMORIA
+e = Thread(target=enviar)
+
+r.start() #INFORMA PARA O SERVICO DE THREAD QUE IRA RODAR
+e.start()
+
+r.join() # MANDA RODAR
+e.join()
+
+# while True:
+#     #recebendo msg do cliente
+#     # cliente.setblocking(False)
+#     # msg = json.loads(cliente.recv(10240).decode('utf-8'))
+#     # print(f"{msg['name']}: {msg['msg']}, Horário: {msg['hora']}")
+#     # print(datetime.datetime.strptime(msg['hora'],'%Y-%m-%d %H:%M:%S.%f'))
     
-    #adicionando no banco]
-    session = sessionmaker(bind=database)()
-    session.add(User(name=msg['name'],mensagem=msg['msg'],horario= datetime.datetime.strptime(msg['hora'],'%Y-%m-%d %H:%M:%S.%f')))
-    session.add(User(name=username,mensagem=server_input,horario=datetime.datetime.now()))
+#     #enviando msg pro cliente
+#     server_input = input('Resposta: ')
+#     msg_env = '{' + f''' "name":"{username}",
+#                "msg":"{server_input}",
+#                "hora":"{datetime.datetime.now()}"
+#                ''' + '}'
+#     cliente.send(msg_env.encode('utf-8'))
     
-    session.commit()
+#     # #adicionando no banco]
+#     # session = sessionmaker(bind=database)()
+#     # session.add(User(name=msg['name'],mensagem=msg['msg'],horario= datetime.datetime.strptime(msg['hora'],'%Y-%m-%d %H:%M:%S.%f')))
+#     # session.add(User(name=username,mensagem=server_input,horario=datetime.datetime.now()))
+    
+#     # session.commit()
